4a41ef5b5fd2c7482c8248f74c517868ce4472e2
stefanceranic <stefanr.ceranic@gmail.com>
2017-06-27 19:49:56 +0200

    Vracanje preseka

--- src/main/java/firma/servisiImplementacija/ZahtevServisImpl.java
+++ src/main/java/firma/servisiImplementacija/ZahtevServisImpl.java
@@ -4,13 +4,12 @@ import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 import firma.model.Banka;
 import firma.model.Racun;
+import firma.presek.GetPresekResponse;
 import firma.repozitorijumi.RacunRepozitorijum;
 import firma.servisi.ZahtevServis;
 import firma.zahtev.GetZahtevRequest;
@@ -30,21 +29,21 @@ public class ZahtevServisImpl implements ZahtevServis {
 	WebServiceTemplate webServiceTemplate;
 
 	@Override
-	public ResponseEntity<?> posaljiZahtev(Zahtev z) {
+	public GetPresekResponse posaljiZahtev(Zahtev z) {
 		System.out.println("firma zahtev servis impl");
 		Racun r = racunRep.findByBrojRacuna(z.getBrojRacuna());
 		Banka b = r.banka;
 		System.out.println(z.getDatumZahteva());
 		GetZahtevRequest zahtev = new GetZahtevRequest();
-	
+
 		zahtev.setZahtev(z);
-	    System.out.println(zahtev.getZahtev().getDatumZahteva());
+		System.out.println(zahtev.getZahtev().getDatumZahteva());
 		String uri = "http://localhost:" + b.port + "/ws";
 		System.out.println(" uri: " + uri);
 		webServiceTemplate.setDefaultUri(uri);
-		webServiceTemplate.marshalSendAndReceive(zahtev);
+		GetPresekResponse gp = (GetPresekResponse) webServiceTemplate.marshalSendAndReceive(zahtev);
 
-		return new ResponseEntity<>(HttpStatus.OK);
+		return gp;
 	}
 
 }
